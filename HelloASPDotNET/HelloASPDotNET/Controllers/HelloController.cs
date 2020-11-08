a022e8bb972131599d553cd3c8c4ae51e882f0a8
Sam <samtpomarico@gmail.com>
2020-11-08 12:37:24 -0600

    First commit.

---
+++ HelloASPDotNET/HelloASPDotNET/Controllers/HelloController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPDotNET.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+
+        public IActionResult Index()
+        {
+            string html = "<form method='post'>" +
+                "<input type = 'text' name = 'name' />" +
+                "<input type = 'submit' value = 'Greet Me!' />" +
+                "</form>";
+            return Content(html, "text/html");
+        }
+
+        [HttpPost]
+        [Route("/hello")]
+        public IActionResult Welcome(string name = "World")
+        {
+            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
+    }
+}
